d94d5b40352dbd40dcbc67a435cf5b58aacfa31a
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-15 19:12:35 +0700

    feat: home page

---
+++ web/features/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="d-flex flex-row">
+      <div className="flex-fill">
+        <h1>Home</h1>
+      </div>
+      <div className="flex-fill">
+        <input type="text" />
+      </div>
+    </div>
+  );
+}

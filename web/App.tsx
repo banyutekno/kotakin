6de4c165c1698a3e1b6012d6b8faa39c1f6bd259
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-21 19:01:16 +0700

    fix: implement lazy loading for App Store

--- web/App.tsx
+++ web/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import { BoxProvider } from './contexts/BoxProvider';
-import AppStore from './features/AppStore';
 
 const Home = lazy(() => import('./features/Home'));
+const Store = lazy(() => import('./features/AppStore'));
+
 function App() {
   return (
     <BoxProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/store" element={<AppStore />} />
+          <Route path="/store" element={<Store />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>

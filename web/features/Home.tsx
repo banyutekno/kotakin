0888b24ad81443c48025022ad1324335e8922558
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-22 11:30:38 +0700

    style: styling app

--- web/features/Home.tsx
+++ web/features/Home.tsx
@@ -9,22 +9,22 @@ export default function Home() {
     <>
       <div className="home-wrapper p-5">
         <Search />
-        <div className="p-3">
-          <div className="d-inline-block rounded p-4 column-bg">
-            <Link to="/store" className="text-decoration-none text-reset">
+        <div className="p-3 d-flex gap-3">
+          <Link to="/store" className="text-decoration-none text-reset">
+            <div className="d-inline-block rounded p-4 column-bg">
               <i className="bi bi-google-play text-light fs-1 ms-3" />
               <p className="mb-0">App Store</p>
-            </Link>
+            </div>
+          </Link>
+          <div className="d-inline-block rounded p-4 column-bg">
+            {boxes?.map((box) => {
+              return (
+                <div key={box.name}>
+                  {box.name} {box.kind} {box.containers.length}
+                </div>
+              );
+            })}
           </div>
-        </div>
-        <div>
-          {boxes?.map((box) => {
-            return (
-              <div key={box.name}>
-                {box.name} {box.kind} {box.containers.length}
-              </div>
-            );
-          })}
         </div>
       </div>
     </>

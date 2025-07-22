591492625f1a851c8ba42c193bd5acc309018e99
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-22 12:34:51 +0700

    feat: introduce clear button to search input

--- web/features/components/Search.tsx
+++ web/features/components/Search.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+
 export function Search() {
+  const [search, setSearch] = useState('');
+
+  const handleClearSearch = () => {
+    setSearch('');
+  };
+
   return (
-    <div className="input-group">
-      <input className="form-control" type="text" placeholder="Search..." />
+    <div className="input-group position-relative">
       <span className="input-group-text">
-        <i className="bi bi-search text-light" />
+        <i className="bi bi-search text-secondary" />
       </span>
+      <input
+        type="text"
+        className="form-control pe-5"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {search && (
+        <button
+          type="button"
+          className="btn position-absolute end-0 top-0 bottom-0 d-flex align-items-center px-3 border-0 bg-transparent"
+          style={{ zIndex: 5 }}
+          onClick={handleClearSearch}
+        >
+          <i className="bi bi-x-lg text-secondary" />
+        </button>
+      )}
     </div>
   );
 }

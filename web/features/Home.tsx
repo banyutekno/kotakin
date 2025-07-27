57875ceddac0a0169d0820353757e6f0fc8ec016
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-28 01:49:00 +0700

    fix: remove back arrow from navbar

--- web/features/Home.tsx
+++ web/features/Home.tsx
@@ -23,14 +23,8 @@ export default function Home() {
 
   return (
     <>
-      <nav className="p-2 column-bg">
+      <nav className="p-3 column-bg">
         <div className="d-flex align-items-center justify-content-between w-100">
-          <Link to="/" className="text-decoration-none text-reset">
-            <div className="ms-3">
-              <i className="bi bi-arrow-left-short fs-1" />
-            </div>
-          </Link>
-
           <div className="flex-grow-1 d-flex justify-content-center">
             <div className="w-50">
               <Search />

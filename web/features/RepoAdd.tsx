a45afaa625bdcca740effa7ddeb42bbb1b02bb9a
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-07-29 14:12:47 +0700

    style: styling repo add

--- web/features/RepoAdd.tsx
+++ web/features/RepoAdd.tsx
@@ -1,6 +1,6 @@
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { Button, FormLabel } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { addRepo } from '../services/repo';
 
 interface FormValues {
@@ -22,20 +22,40 @@ export default function RepoAdd() {
   };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Add Repository</h1>
-
-        <div className="mb-3">
+    <div className="container py-5">
+      <div className="column-bg card shadow rounded-4 px-4 py-4">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="d-flex justify-content-between align-items-center mb-2">
+            <h2 className="mb-4 ">
+              <i className="bi bi-folder me-2" />
+              Add Repository
+            </h2>
+            <Link to="/store" className="text-decoration-none text-reset">
+              <Button variant="secondary">
+                <i className="bi bi-arrow-left-short fs-5" />
+                Back
+              </Button>
+            </Link>
+          </div>
           <div className="mb-3">
             <FormLabel>URL</FormLabel>
-            <input type="text" className="form-control" {...register('url', { required: 'URL is required' })} />
-            {errors.url && <p>{errors.url.message}</p>}
+            <input
+              type="text"
+              className={`form-control ${errors.url ? 'is-invalid' : ''}`}
+              placeholder="Enter repository URL"
+              {...register('url', { required: 'URL is required' })}
+            />
+            {errors.url && <div className="invalid-feedback">{errors.url.message}</div>}
           </div>
-        </div>
 
-        <Button type="submit">Add Repo</Button>
-      </form>
+          <div className="mt-4">
+            <Button type="submit" variant="primary" className="w-100">
+              <i className="bi bi-plus me-2" />
+              Add Repo
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

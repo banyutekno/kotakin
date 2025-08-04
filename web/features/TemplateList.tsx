97539617ec5fb024d974612c5363613586b9aae6
Revanza Firdaus <revanza.firdaus@gmail.com>
2025-08-04 12:07:12 +0700

    fix: navbar for store

--- web/features/TemplateList.tsx
+++ web/features/TemplateList.tsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import { getTemplates } from '../services/template';
-import { Navbar } from './components/Navbar';
 
 interface Template {
   id: string;
@@ -22,7 +21,26 @@ export default function TemplateList() {
 
   return (
     <>
-      <Navbar showBackIcon />
+      <nav className="nav-store-bg p-2">
+        <div className="container-fluid">
+          <div className="row align-items-center">
+            <div className="col-6 col-md-3 d-flex justify-content-start mb-3 mb-md-0">
+              <Link to="/" className="text-decoration-none text-reset">
+                <div className="rounded-circle back-btn d-flex align-items-center justify-content-center">
+                  <i className="bi bi-arrow-left-short fs-1" />
+                </div>
+              </Link>
+            </div>
+
+            <div className="col-6 col-md-9 d-flex justify-content-end">
+              <Link to="/repo/-/add">
+                <Button>Add Repository</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
       <div>
         {templates.map((template) => (
           <div key={template.id} className="border p-2 m-2">
